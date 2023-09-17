c44d443a61601b679a617f78cf676990950a1db9
tarun <tarun@LAPTOP-384VCLUN>
2023-09-17 18:48:04 +0530

    Add project files.

---
+++ ass4_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass4_2
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        
+        public string FullCarName
+        {
+            get
+            {
+                return $"{Make} {Model} {Year}";
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car
+            {
+                Make = "Toyota",
+                Model = "Camry",
+                Year = 2022
+            };
+
+            Console.WriteLine(myCar.FullCarName);
+        }
+    }
+}
